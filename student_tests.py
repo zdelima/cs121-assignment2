58519ff697ef363233fe562d0f5b970cb298daf9
zdelima <zdelima@uci.edu>
2025-04-29 18:24:04 -0700

    Modified is_valid to check domains per assignment and added unit testing

---
+++ student_tests.py
@@ -0,0 +1,16 @@
+import unittest
+from scraper import *
+
+class TestScraper(unittest.TestCase):
+    def test_is_valid(self):
+        self.assertTrue(is_valid("http://test.ics.uci.edu/robots.txt"))
+        self.assertTrue(is_valid("http://test.cs.uci.edu/robots.txt"))
+        self.assertTrue(is_valid("http://test.informatics.uci.edu/robots.txt"))
+        self.assertTrue(is_valid("http://test.stat.uci.edu/robots.txt"))
+        self.assertTrue(is_valid("http://today.uci.edu/department/information_computer_sciences/robots.txt"))
+        self.assertFalse(is_valid("http://today.uci.edu/cs121/information_computer_sciences/robots.txt"))
+        self.assertFalse(is_valid("https://(canvas.eee.uci.edu/courses/72511/assignments/1584020"))
+
+    
+if __name__ == '__main__':
+    unittest.main()
