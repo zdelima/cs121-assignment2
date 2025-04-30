0e953533370051b329be967b80ad176aa8568542
zdelima <zdelima@uci.edu>
2025-04-29 21:18:23 -0700

    Add back checks for non html files

--- scraper.py
+++ scraper.py
@@ -58,6 +58,17 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]): # if scheme isnt either http/https
             return False
+        if re.match(
+            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
+            return False
+
         if parsed.netloc.endswith(("ics.uci.edu", ".cs.uci.edu", ".information.uci.edu", ".stat.uci.edu")): # if one of the allowed domains
             return True
         if parsed.netloc == "today.uci.edu": # if specifically today.uci.edu
